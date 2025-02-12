62c56ef70b91f2691869a560f1f950d2d7e3256d
talhahzahid <talhazahid415@gmail.com>
2025-02-12 12:59:07 +0500

    DataBase Connected

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import dotenv from "dotenv"
+dotenv.config()
 const app = express();
 const port = 9000;
+import connectdb from "./src/db/index.js";
 
 app.get("/", (req, res) => {
   res.send("Ecommerce Server");
 });
 
-app.listen(port, () => {
-  console.log("Server Is Running At Port", port);
-});
+connectdb()
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Server Is Running At Port", port);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
